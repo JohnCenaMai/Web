7c60cd0be73d78521f04ccb23ebb7b9ff5313c8e
Noforgive <maithaison1998@gmail.com>
2019-10-01 10:37:51 +0700

    Web

---
+++ De Thi Cuoi Ky/Transform.js
@@ -0,0 +1,12 @@
+function OnClick() {
+    document.getElementById("id1").innerHTML = "Revenger";
+}
+
+function GetWidth() {
+    document.getElementById("id2").innerHTML = window.innerWidth;
+}
+
+function GetHeight() {
+    document.getElementById("id2").innerHTML = window.innerHeight;
+}
+
